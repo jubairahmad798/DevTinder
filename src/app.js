f51fcbc286f977b58aa892b7b94f4617c6e0e293
Jubairahmad798 <jubairahmad798@gmail.com>
2026-04-29 10:29:06 +0530

    Dynamic data signup-api

--- src/app.js
+++ src/app.js
@@ -4,17 +4,27 @@ const { connectDB } = require("./config/database"); // Import database connectio
 const User = require ("./models/user.js");
 require("dotenv").config();
 
+app.use(express.json()); // covert for all incoming req into js object 
+// Middleware that parses incoming JSON request bodies
+// and converts them into JavaScript objects.
+// After this, client-sent data becomes available in req.body.
+
 
 
 
 app.post ("/signup", async (req, res)=>{
 
-    const user = new User({
-        firstName :"Jubair2",
-        lastName : "Ahmad",
-        emailId : "jubair2@gmail.com",
-        password :"jubair1232"
-    })
+ // console.log(req.body); // read the data in the console which is sending from the postman as an client 
+    // Displays the data sent by the client (Postman/frontend)
+    // Useful for testing and debugging incoming request data.
+
+
+    const user = new User(req.body); //  Now here we make data dynamic means making instance of an oject which sending from postman by this we will able to save data in database
+      // Create a new User model instance using client data.
+    // req.body contains dynamic values sent in request.
+    // This object follows the schema structure
+    // and can now be saved into MongoDB.
+
 try{
      await user.save();
      res.send("user Added Successfully to the Database !!✅");
